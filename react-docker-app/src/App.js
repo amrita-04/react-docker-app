542b0fb09468ba2987f214ca2e60941b003f1f39
amrita-04 <amritaraman04@gmail.com>
2026-04-15 07:25:58 +0530

    initial commit with ci-cd

---
+++ react-docker-app/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React Docker CI/CD App</h1>
+      <p>It is working!</p>
+    </div>
+  );
+}
+
+export default App;
